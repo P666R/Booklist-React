22c97d2c7d33753eb9956e9b932d8e3fc581796d
PRoy <sam469roger@gmail.com>
2023-03-25 02:35:03 +0530

    refactored the code

--- src/books.js
+++ src/books.js
@@ -1,27 +1,32 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+import img4 from "./images/book-4.jpg";
+
 export const books = [
   {
     title: "Dog Man: Twenty Thousand Fleas Under the Sea",
     author: "Dav Pilkey",
-    img: "./images/book-1.jpg",
+    img: img1,
     id: 1,
   },
   {
     title:
       "Eat to Beat Your Diet: Burn Fat, Heal Your Metabolism, and Live Longer",
     author: "William W Li MD",
-    img: "./images/book-2.jpg",
+    img: img2,
     id: 2,
   },
   {
     title: "Lessons in Chemistry: A Novel",
     author: "Bonnie Garmus",
-    img: "./images/book-3.jpg",
+    img: img3,
     id: 3,
   },
   {
     title: "It's Not Easy Being a Bunny (Beginner Books(R))",
     author: "Marilyn Sadler",
-    img: "./images/book-4.jpg",
+    img: img4,
     id: 4,
   },
 ];
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return <Book key={book.id} {...book} />;
-      })}
-    </section>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book key={book.id} number={index} {...book} />;
+        })}
+      </section>
+    </>
   );
 };
 
