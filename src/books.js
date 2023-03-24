ce5c4787d9c58b28ce24764a082be14d195e0571
PRoy <sam469roger@gmail.com>
2023-03-25 02:01:34 +0530

    added exports and imports

---
+++ src/books.js
@@ -0,0 +1,27 @@
+export const books = [
+  {
+    title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+    author: "Dav Pilkey",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title:
+      "Eat to Beat Your Diet: Burn Fat, Heal Your Metabolism, and Live Longer",
+    author: "William W Li MD",
+    img: "./images/book-2.jpg",
+    id: 2,
+  },
+  {
+    title: "Lessons in Chemistry: A Novel",
+    author: "Bonnie Garmus",
+    img: "./images/book-3.jpg",
+    id: 3,
+  },
+  {
+    title: "It's Not Easy Being a Bunny (Beginner Books(R))",
+    author: "Marilyn Sadler",
+    img: "./images/book-4.jpg",
+    id: 4,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
-};
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg"
-    alt="img"
-  />
-);
-
-const Title = () => {
-  return <h2>Dog Man: Twenty Thousand Fleas Under the Sea</h2>;
-};
-
-const Author = () => {
-  return <h4>Dav Pilkey</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
